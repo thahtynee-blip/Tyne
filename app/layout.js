53e61e2cfcdbd30cc91d1e5ca698cce365999066
Đỗ Thị Thanh Tuyền <dothithanhtuyendq2003@MacBook-Tyne.local>
2026-07-29 14:56:37 +0700

    Integrate Supabase SDK and apply design changes

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="vi">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,300..900;1,9..144,300..900&family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
       <body>
